1bc04f1d5b9f496938e1ca2bdf22b86b39aad238
Joed Parin <parinjoed@gmail.com>
2019-10-29 05:55:13 +0800

    feat: added get function with mock test get event

---
+++ get.js
@@ -0,0 +1,27 @@
+import { call } from "./libs/dynamodb-lib";
+import { failure, success } from './libs/response-lib';
+
+export async function main(event) {
+    const params = {
+        TableName: process.env.tableName,
+        Key: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: event.pathParameters.id
+        }
+    };
+
+    try {
+        const result = await call("get", params);
+        if (result.Item) {
+            // Return the retrieved item
+            return success(result.Item);
+        }
+        else
+        {
+            return failure({ status: false, error: "Item not found." });
+        }
+    } catch (e) {
+        return failure({ status: false });
+    }
+
+}
