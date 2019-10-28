a7e258c88f515da66fdd2e3e693c80ed7ba964d1
Joed Parin <parinjoed@gmail.com>
2019-10-29 06:35:32 +0800

    feat: added delete function with mock test delete event

---
+++ delete.js
@@ -0,0 +1,21 @@
+import { call } from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
+export async function main(event) {
+
+    const params = {
+        TableName: process.env.tableName,
+        Key: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: event.pathParameters.id
+        }
+    };
+
+    try {
+        await call('delete', params);
+        return success({status: true });
+    } catch (err) {
+        // Not the best approach to log an error
+        console.log(err);
+        return failure({ status: false });
+    }
+}
