eaa7395615b6a8f24c3fbfae70c2f7d53acc8e4f
Joed Parin <parinjoed@gmail.com>
2019-11-04 17:39:50 +0800

    Bug: corrected stripesecretkey fieldname

--- billing.js
+++ billing.js
@@ -3,12 +3,11 @@ import { calculateCost } from "./libs/billing-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-  const { storage, source } = JSON.parse(event.body);
+  const { storage, source, cardDetail } = JSON.parse(event.body);
   const amount = calculateCost(storage);
-  const description = "Scratch charge";
+  const description = "Brand: "+cardDetail.brand+", Card Holders Name: "+cardDetail.cardHoldersName;
   // Load our secret key from the environment variables
-  const stripe = stripePackage(process.env.stripeSecretKey);
-
+  const stripe = stripePackage(process.env.StripeSecretKey);
   try {
     await stripe.charges.create({
       source,
